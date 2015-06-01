57c8b0227667a53993b6995ffd854248f9137ac6
pyar6329 <pyar6329@gmail.com>
2015-06-02 04:38:11 +0900

    add plus method

--- lib/plus.rb
+++ lib/plus.rb
@@ -3,12 +3,14 @@ require './arithmeticOp'
 class Plus < ArithmeticOp
 
   private
+
     def initialize(left,right)
       @left = left
       @right = right
     end
 
   public
+
     def execute
       @left + @right
     end
