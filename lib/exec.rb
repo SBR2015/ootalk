ab319ada71db422471ab38bd4bbd399c4f5acaa3
pyar6329 <pyar6329@gmail.com>
2015-06-02 04:36:59 +0900

    add plus method

--- lib/exec.rb
+++ lib/exec.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
 require './program'
+require './plus'
 # Dir[File.expand_path('./lib', __FILE__) << '/*.rb'].each do |file|
 #   require file
 # end
 
-p = Program.new
-puts "hello"
+# p = Program.new
+
+a = Plus.new(1,2)
+b = Plus.new(3,4)
+
+c = Plus.new(a.execute,b.execute)
+
+puts "a = #{a.execute}"
+puts "b = #{b.execute}"
+puts "a+b = #{c.execute}"
--- lib/plus.rb
+++ lib/plus.rb
@@ -1,4 +1,16 @@
 require './arithmeticOp'
 
 class Plus < ArithmeticOp
+
+  private
+    def initialize(left,right)
+      @left = left
+      @right = right
+    end
+
+  public
+    def execute
+      @left + @right
+    end
+
 end
