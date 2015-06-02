9b5ea982c72df70b173c5bc67f6e0317d92566fa
pyar6329 <pyar6329@gmail.com>
2015-06-02 18:11:29 +0900

    add to_s method to plus

--- lib/plus.rb
+++ lib/plus.rb
@@ -2,17 +2,21 @@ require './arithmeticOp'
 
 class Plus < ArithmeticOp
 
-  # private
-  #
-  #   def initialize(left,right)
-  #     @left = left
-  #     @right = right
-  #   end
-  #
-  # public
-  #
-  #   def execute
-  #     @left + @right
-  #   end
+  private
+
+    def initialize(left,right)
+      @left = left
+      @right = right
+    end
+
+  public
+
+    def exec
+      @left + @right
+    end
+
+    def to_s
+      "(#{@left}+#{@right})"
+    end
 
 end
