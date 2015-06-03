62893dbbbea055f5b6df0cb98c61c7016302e014
pyar6329 <pyar6329@gmail.com>
2015-06-03 23:00:48 +0900

    add comment to exec.rb

--- exec.rb
+++ exec.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-# Libraryの読み込み
+# ライブラリの読み込み
 Dir[File.expand_path('../lib', __FILE__) << '/*.rb'].each do |file|
   require file
 end
 
 # p = Program.new
 
-# 数値を作成
+# 定数の宣言
 cons1 = Constant.new(1)
 cons2 = Constant.new(2)
 cons3 = Constant.new(3)
@@ -18,8 +18,11 @@ b = Plus.new(cons1,cons3)
 c = Plus.new(a,b)
 # d = Plus.new(c,b)
 
+# 実行
 puts "a = #{a.exec}"
 puts "b = #{b.exec}"
 puts "a + b = #{c.exec}"
+
+# コンパイル
 puts "a + b = #{c.to_s}"
 # puts "d = " + d.to_s
