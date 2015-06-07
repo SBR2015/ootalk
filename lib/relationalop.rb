8a8733b7b3e1a5a3d103314a7a5c1a20e134216a
Yusuke Ohashi <yusuke@junkpiano.me>
2015-06-07 22:40:48 +0900

    add relational operator class.

---
+++ lib/relationalop.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/operator'
+
+class RelationalOp < Operator
+end
---
+++ spec/relationalop_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require_relative '../lib/relationalop.rb'
+
+describe 'Relational Operator' do
+  it "new" do
+    op = RelationalOp.new
+    expect(op).to_not be_nil
+  end
+
+  it "exec" do
+    op = RelationalOp.new
+    expect(op.exec).to be false
+  end
+
+  it "to_s" do
+    op = RelationalOp.new
+    expect(op.to_s.length).to be >= 0
+  end
+
+end
