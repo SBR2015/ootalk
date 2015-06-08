687a9f48f19c5febc5e5b385c926160df1f610c6
shinjukujohnny <kikkawa1323@fout.jp>
2015-06-08 18:19:49 +0900

    delete private, public

--- lib/less_than.rb
+++ lib/less_than.rb
@@ -2,14 +2,10 @@ require File.dirname(__FILE__) + '/arithmeticOp'
 
 class LessThan < ArithmeticOp
 
-  private
-
     def initialize(left, right)
       super
       @operator = '<'
     end
-
-  public
 
     def exec
       @left.exec < @right.exec
