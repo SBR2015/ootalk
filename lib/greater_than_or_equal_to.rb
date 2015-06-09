4d0ea0ee755a7782897810cfa70726ba896ff712
Bao Linh <masusapu@gmail.com>
2015-06-09 23:21:24 +0700

    create GreaterThanOrEqualTo class

---
+++ lib/greater_than_or_equal_to.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/relationalop'
+
+class GreaterThanOrEqualTo < RelationalOp
+
+  def initialize(left, right)
+    super
+    @operator = '>='
+  end
+
+  def exec
+    @left.exec >= right.exec
+  end
+end
