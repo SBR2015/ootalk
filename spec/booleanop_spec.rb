e67b46a063af718932c2a53028838b784619d2a1
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-06-09 12:53:10 +0000

    fix error

--- spec/booleanop_spec.rb
+++ spec/booleanop_spec.rb
@@ -4,12 +4,12 @@ require_relative '../lib/boolean_op.rb'
 describe 'Boolean Operator' do
   it "new" do
     bop = BooleanOp.new
-    expect(op).to_not be_nil
+    expect(bop).to_not be_nil
   end
 
   it "to_s" do
     bop = BooleanlOp.new
-    expect(op.to_s.length).to be >= 0
+    expect(bop.to_s.length).to be >= 0
   end
 
 end
