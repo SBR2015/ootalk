d7f248a94e04206fe4b0a3452e29ee9bf2d08262
pyar6329 <pyar6329@gmail.com>
2015-06-10 16:30:54 +0900

    create i18n

---
+++ locale_test.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'i18n'
+
+# ライブラリの読み込み
+# Dir[File.expand_path('../config/locales', __FILE__) << '/*.yml'].each do |file|
+#   require file
+# end
+
+# i18n = :ja
+puts "hello"
