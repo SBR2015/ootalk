a9754cfacf886b8ae22b7b4ccc7376feeac70169
pyar6329 <pyar6329@gmail.com>
2015-06-10 17:16:23 +0900

    create i18n

--- locale_test.rb
+++ locale_test.rb
@@ -1,11 +1,41 @@
 #!/usr/bin/env ruby
 
-require 'i18n'
+# $ bundle exec ruby locales_test.rb
 
 # ライブラリの読み込み
-# Dir[File.expand_path('../config/locales', __FILE__) << '/*.yml'].each do |file|
-#   require file
+require 'json'
+require 'i18n'
+I18n.load_path = Dir[File.expand_path('../config/locales', __FILE__) << '/*.yml']
+
+# I18n.backend.load_translations
+
+# 言語設定
+I18n.locale = :ja
+
+lang_json = {
+  Plus: {
+    id: 1,
+    name: I18n.t('Plus.name'),
+    string: I18n.t('Plus.string')
+  },
+  Minus: {
+    id: 2,
+    name: I18n.t('Minus.name'),
+    string: I18n.t('Minus.string')
+  }
+}
+
+# 実際には↓のように実装していく
+# class_all_lists = "クラスリストをループで取得"
+# lang_json = []
+#
+# class_all_lists.each do |c|
+#   this_class = {}
+#   this_class[:id] = "ループ回数の値"
+#   this_class[:name] = I18n.t('c.name')
+#   this_class[:string] = I18n.t('c.string')
+#
+#   lang_json.push(this_class)
 # end
 
-# i18n = :ja
-puts "hello"
+puts lang_json.to_json
