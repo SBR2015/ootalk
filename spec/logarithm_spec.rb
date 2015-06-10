cd41cfaeac64f0c819f3519574259a59f4c6d613
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-06-10 20:25:20 +0900

    add raise_error

--- spec/logarithm_spec.rb
+++ spec/logarithm_spec.rb
@@ -17,18 +17,21 @@ describe "logarithms" do
     cons4 = Constant.new(8)
     cons5 = Constant.new(0.2)
     cons6 = Constant.new(0.04)
+    cons7 = Constant.new(-2)
     
     logarithms1 = Logarithm.new(cons1, cons3)
     logarithms2 = Logarithm.new(cons2, cons4)
     logarithms3 = Logarithm.new(cons5, cons6)
     logarithms4 = Logarithm.new(cons1, cons5)
     logarithms5 = Logarithm.new(cons6, cons3)
+    logarithms6 = Logarithm.new(cons7, cons2)
 
     expect(logarithms1.exec).to be == 0.47712125471966244
     expect(logarithms2.exec).to be == 3.0 
     expect(logarithms3.exec).to be == 2.0
     expect(logarithms4.exec).to be == -0.6989700043360187
     expect(logarithms5.exec).to be == -0.3413030972429927
+    expect(logarithms6.exec).to raise_error 
 
   end
 end
