e85aab46d39ee53497efa780b2db0343013d492a
Ounennhei <wnping123@yahoo.co.jp>
2015-06-18 00:52:59 +0900

    fix assignment

--- lib/ootalk/assignment.rb
+++ lib/ootalk/assignment.rb
@@ -5,8 +5,20 @@ module OoTalk
   class Assignment < Instruction
     private
 
-      def initialize
-        @target = Variable.new
-      end
+    def initialize(target = nil, source = nil)
+      @target = target
+      @source = source
+      @op = '='
+    end
+
+    public
+
+    def exec
+      OoTalk::Variable.name[@target.to_s] = @source.exec
+    end
+
+    def to_s
+      @target.to_s
+    end
   end
 end
