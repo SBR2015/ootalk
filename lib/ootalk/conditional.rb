8a18f42f5c1c4ec216fc9b4e4d9bbfcd363289ab
Bao Linh <masusapu@gmail.com>
2015-06-20 01:54:07 +0900

    create Conditional class + test

--- lib/ootalk/conditional.rb
+++ lib/ootalk/conditional.rb
@@ -4,9 +4,28 @@ module OoTalk
   class Conditional < Instruction
     private
 
-      def initialize
-        @thenbranch = Instruction.new
-        @elsebranch = Instruction.new
+      def initialize(condition, thenbranch, elsebranch = nil)
+        @condition = condition
+        @thenbranch = thenbranch
+        @elsebranch = elsebranch
+      end
+
+    public
+
+      def to_s
+        if @elsebranch == nil
+          "if #{@condition.to_s}\n\t#{@thenbranch}\nend"
+        else
+          "if #{@condition.to_s}\n\t#{@thenbranch}\nelse\n\t#{@elsebranch}\nend"
+        end
+      end
+
+      def exec
+        if @condition.exec
+          @thenbranch.exec
+        elsif @elsebranch != nil
+          @elsebranch.exec
+        end
       end
   end
 end
---
+++ spec/conditional_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'ootalk'
+
+describe "Ootalk::Conditional class" do
+  var = OoTalk::Variable.new('a')
+  ass = OoTalk::Assignment.new(var, OoTalk::Constant.new(1))
+
+  equal_1 = OoTalk::EqualTo.new(var, OoTalk::Constant.new(1))
+  not_equal_1 = OoTalk::NotEqual.new(var, OoTalk::Constant.new(1))
+
+  increment = OoTalk::Assignment.new(var, OoTalk::Add.new(var, OoTalk::Constant.new(1)))
+  decrement = OoTalk::Assignment.new(var, OoTalk::Substract.new(var, OoTalk::Constant.new(1)))
+
+  cond1 = OoTalk::Conditional.new(equal_1, increment)
+  cond2 = OoTalk::Conditional.new(not_equal_1, increment, decrement)
+
+  it "should be initialized properly" do
+    expect(cond1).to_not be_nil
+    expect(cond2).to_not be_nil
+  end
+
+  it "should execute properly" do
+    # a = 1; if a == 1 then a += 1
+    ass.exec
+    cond1.exec
+    expect(var.exec).to be 2
+
+    # a = 1; if a != 1 then a += 1 else a -= 1
+    ass.exec
+    cond2.exec
+    expect(var.exec).to be 0
+  end
+
+  it "should execute properly" do
+    expect(cond1.to_s).to eq("if (a==1)\n\ta=(a+1)\nend")
+    expect(cond2.to_s).to eq("if (a!=1)\n\ta=(a+1)\nelse\n\ta=(a-1)\nend")
+  end
+end
