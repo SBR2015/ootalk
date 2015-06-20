f2302e1d451e71aef7d8248b0826412cd7c4a0f0
a1447ll <masusapu@gmail.com>
2015-06-20 13:40:38 +0900

    Update conditional.rb

--- lib/ootalk/conditional.rb
+++ lib/ootalk/conditional.rb
@@ -4,25 +4,22 @@ module OoTalk
   class Conditional < Instruction
     private
 
-      def initialize(condition, thenbranch, elsebranch = nil)
-        @condition = condition
-        @thenbranch = thenbranch
-        @elsebranch = elsebranch
-      end
+    def initialize(condition, thenbranch, elsebranch = nil)
+      @condition = condition
+      @thenbranch = thenbranch
+      @elsebranch = elsebranch
+    end
 
     public
 
-      def to_s
-        if @elsebranch == nil
-          "if #{@condition.to_s}\n\t#{@thenbranch}\nend"
-        else
-          "if #{@condition.to_s}\n\t#{@thenbranch}\nelse\n\t#{@elsebranch}\nend"
-        end
-      end
+    def to_s
+      return "if #{@condition.to_s}\n\t#{@thenbranch}\nend" if @elsebranch.nil?
+      return "if #{@condition.to_s}\n\t#{@thenbranch}\nelse\n\t#{@elsebranch}\nend"
+    end
 
-      def exec
-        return @thenbranch.exec if @condition.exec 
-        return @elsebranch.exec unless @elsebranch.nil?
-      end
+    def exec
+      return @thenbranch.exec if @condition.exec
+      return @elsebranch.exec unless @elsebranch.nil?
+    end
   end
 end
