ee22a2a6d3802633878ea08beae67099303f6942
Ounennhei <wnping123@yahoo.co.jp>
2015-06-22 20:06:57 +0900

    add Skip class

--- lib/ootalk/skip.rb
+++ lib/ootalk/skip.rb
@@ -2,5 +2,21 @@ require 'ootalk/instruction'
 
 module OoTalk
   class Skip < Instruction
+
+    private
+
+    def initialize(comment)
+      @comment = comment
+    end
+
+    public
+
+    def exec
+    end
+
+    def to_s
+      return "# #{@comment.to_s}"
+    end
+
   end
 end
