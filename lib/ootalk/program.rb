8916bfffc1fcac50e910cf01b986560be72302f1
Yusuke Ohashi <yusuke@junkpiano.me>
2015-06-22 16:15:21 +0900

    added executable command "ootalk" (proudly). added skeleton program class.

--- lib/ootalk/program.rb
+++ lib/ootalk/program.rb
@@ -2,10 +2,20 @@ require 'ootalk/instruction'
 
 module OoTalk
   class Program
-    private
+    def initialize(data)
+      @body = data
+    end
 
-      def initialize
-        @body = Instruction.new
-      end
+    def raw_data
+      @body
+    end
+
+    def exec
+      '{result: "test"}'
+    end
+
+    def to_s
+      '{Result: "test"}'
+    end
   end
 end
