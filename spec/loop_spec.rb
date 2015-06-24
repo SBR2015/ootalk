db1e5072fc01c4f9817fdbeb99f3bc57936e0156
shinjukujohnny <kikkawa1323@fout.jp>
2015-06-25 00:58:19 +0900

    変数名が他のテストと被ると、他のテストに影響してしまうため、変数名変更

--- spec/loop_spec.rb
+++ spec/loop_spec.rb
@@ -14,32 +14,33 @@ describe 'Loop' do
   end
 
   it 'exec' do
-    #while (x < 3) do
-    #    y += 10
+    #i = 0
+    #while (i < 3) do
+    #    a += 10
     #end
-    #y => 30
+    #a => 30
     cons3 = OoTalk::Constant.new(3)
     cons10 = OoTalk::Constant.new(10)
 
-    # x = 0
-    val_x = OoTalk::Variable.new('x')
+    # i = 0
+    val_i = OoTalk::Variable.new('i')
     cons0 = OoTalk::Constant.new(0)
-    OoTalk::Assignment.new(val_x, cons0).exec
-    # x + 1
+    OoTalk::Assignment.new(val_i, cons0).exec
+    # i + 1
     cons1 = OoTalk::Constant.new(1)
-    add = OoTalk::Add.new(val_x, cons1)
-    # x = x + 1
-    assign = OoTalk::Assignment.new(val_x, add)
-    # x < 3
-    less_than = OoTalk::LessThan.new(val_x, cons3)
-    # y = 0
-    val_y = OoTalk::Variable.new('y')
-    OoTalk::Assignment.new(val_y, cons0).exec
-    # y = y + 10
-    add = OoTalk::Add.new(val_y, cons10)
-    results = OoTalk::Assignment.new(val_y, add)
+    add = OoTalk::Add.new(val_i, cons1)
+    # i = i + 1
+    assign = OoTalk::Assignment.new(val_i, add)
+    # i < 3
+    less_than = OoTalk::LessThan.new(val_i, cons3)
+    # a = 0
+    val_a = OoTalk::Variable.new('a')
+    OoTalk::Assignment.new(val_a, cons0).exec
+    # a = a + 10
+    add = OoTalk::Add.new(val_a, cons10)
+    results = OoTalk::Assignment.new(val_a, add)
     lp = OoTalk::Loop.new(less_than, results, assign)
     lp.exec
-    expect(val_y.exec).to be 30
+    expect(val_a.exec).to be 30
   end
 end
