1e72ae8a271c20ad01c8bf8f305878aed5493a6b
Yusuke Ohashi <github@junkpiano.me>
2015-11-20 14:50:04 +0900

    remove i18n dependency.
    - #110
    - #111

--- lib/ootalk/abstract_syntax_lists.rb
+++ lib/ootalk/abstract_syntax_lists.rb
@@ -1,42 +1,27 @@
-# ライブラリの読み込み
-require 'i18n'
 require 'yaml'
 
 module OoTalk
   class AbstractSyntaxLists
     def self.create(locale = :en)
-      # 言語設定
-      I18n.load_path += Dir[File.expand_path('../../../config/locales', __FILE__) << '/*.yml']
-      I18n.available_locales = [:en, :ja, :cn, :vi]
-      I18n.enforce_available_locales = true
-      I18n.default_locale = :en
-
-      I18n.locale = locale
-
       # 抽象文法のリストを取得
-      # abstract_syntax = YAML.load_file(File.dirname(__FILE__) +
-      #                                 "/../../config/locales/as.#{I18n.locale}.yml")
-      load_file = File.expand_path('../../../config/locales', __FILE__) << "/as.#{I18n.locale}.yml"
+      load_file = File.expand_path('../../../config/locales', __FILE__) << "/as.#{locale}.yml"
       abstract_syntax = YAML.load_file(load_file)
 
-      # 抽象文法のクラス一覧を取得
-      class_all_lists = abstract_syntax[I18n.locale.to_s].map do |key, _value|
-        key
-      end
-
-      # 出力用の配列
+      # 抽象文法リストの出力結果を生成
+      i = 0
       options = []
-
-      class_all_lists.each_with_index do |c, i|
+      abstract_syntax[locale.to_s].map do |key, _value|
         this_class = {}
         this_class[:id] = i + 1
-        this_class[:class_name] = c
-        this_class[:name] = I18n.t("#{c}.name")
-        this_class[:string] = I18n.t("#{c}.string")
+        this_class[:class_name] = key
+        this_class[:name] = _value['name']
+        this_class[:string] = _value['string']
         options.push(this_class)
+        i += 1
       end
-
+      puts options
       options
+      
     end
   end
 end
