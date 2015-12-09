99dd30735cd9bed8f1c31d1524e35d24eed13aaf
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-12-09 22:21:44 +0900

    add compound_yaml and loop_yaml

--- lib/ootalk/loop.rb
+++ lib/ootalk/loop.rb
@@ -9,17 +9,15 @@ module Ootalk
     end
 
     # @left.exec: 条件式(loop条件)
-    # @middle.exec: Instruction#exec
-    # @right.exec: 条件式(loop条件の制御)
+    # @right.exec: 実行文
     def exec
       while true do
         break unless @left.exec
-        @middle.exec
         @right.exec
       end
     end
     def to_s
-      "while true do\n  break unless #{@left}\n  #{@middle}\n  #{@right}\nend"
+      "while true do\n  break unless #{@left}\n　#{@right}\nend"
     end
   end
 end
